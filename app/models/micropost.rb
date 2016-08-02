cc85987ed5a9da773e6ec88dedc4bffebfae369a
DienTranVan <tranvandien.k55@hus.edu.vn>
2016-08-02 08:46:08 +0700

    demo app

---
+++ app/models/micropost.rb
@@ -0,0 +1,6 @@
+class Micropost < ApplicationRecord
+  validates :user_id, length: { maximum: 15 },
+ 		      presence: true
+  validates :content, length: { maximum: 140 },
+                      presence: true
+end
